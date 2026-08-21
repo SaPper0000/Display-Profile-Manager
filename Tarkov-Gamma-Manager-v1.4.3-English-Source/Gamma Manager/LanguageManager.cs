a27cb78463188b2d91e71dee0b99665855d4a3eb
SaPper0000 <jungsm0707@gmail.com>
2026-08-22 00:49:56 +0900

    Add files via upload

---
+++ Tarkov-Gamma-Manager-v1.4.3-English-Source/Gamma Manager/LanguageManager.cs
@@ -0,0 +1,8 @@
+namespace Gamma_Manager
+{
+    internal static class LanguageManager
+    {
+        // Build language. This source package is the Korean build.
+        public const bool Korean = false;
+    }
+}
