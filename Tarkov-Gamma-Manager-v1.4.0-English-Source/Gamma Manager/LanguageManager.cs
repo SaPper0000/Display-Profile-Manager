831c9a891bc487a922e29e11687fcba9ab97555a
SaPper0000 <jungsm0707@gmail.com>
2026-08-22 00:48:25 +0900

    Delete Tarkov-Gamma-Manager-v1.4.0-English-Source directory

--- Tarkov-Gamma-Manager-v1.4.0-English-Source/Gamma Manager/LanguageManager.cs
+++
@@ -1,8 +0,0 @@
-namespace Gamma_Manager
-{
-    internal static class LanguageManager
-    {
-        // Build language. This source package is the English build.
-        public const bool Korean = false;
-    }
-}
