620a58eac5704564f542a8cb72694f6d49c61773
SaPper0000 <jungsm0707@gmail.com>
2026-08-31 22:50:52 +0900

    Add files via upload

---
+++ Display-Profile-Manager-v1.5.3-1-source/Gamma Manager/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Assembly metadata for Display Profile Manager v1.5.
+[assembly: AssemblyTitle("Display Profile Manager")]
+[assembly: AssemblyDescription("Display profile and color control utility")]
+[assembly: AssemblyConfiguration("Release 1.5")]
+[assembly: AssemblyCompany("Display Profile Manager")]
+[assembly: AssemblyProduct("Display Profile Manager")]
+[assembly: AssemblyCopyright("Copyright © 2026 Display Profile Manager Project")]
+[assembly: AssemblyTrademark("Display Profile Manager")]
+[assembly: AssemblyCulture("")]
+
+// Keep COM exposure disabled unless explicitly required.
+[assembly: ComVisible(false)]
+
+// Type library identifier.
+[assembly: Guid("2efc2fc6-5d10-40ca-b41c-2e5084ad03a1")]
+
+// Assembly version information.
+[assembly: AssemblyVersion("1.5.3.1")]
+[assembly: AssemblyFileVersion("1.5.3.1")]
